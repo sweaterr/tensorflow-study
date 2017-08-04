58c4482977b3953fd7aeac23d8901259468f10f1
will.link <sweaterr@gmail.com>
2017-08-04 14:56:33 +0900

    Update sparse_tensor_to_dense.py

--- api/sparse_tensor_to_dense.py
+++ api/sparse_tensor_to_dense.py
@@ -2,9 +2,9 @@ st = tf.SparseTensor(indices=[[0, 0], [1, 2]], values=[1, 2], dense_shape=[3, 4]
 
 st.indices, st.values, st.dense_shape
 
-(<tf.Tensor 'SparseTensor_4/indices:0' shape=(2, 2) dtype=int64>,
- <tf.Tensor 'SparseTensor_4/values:0' shape=(2,) dtype=int32>,
- <tf.Tensor 'SparseTensor_4/dense_shape:0' shape=(2,) dtype=int64>)
+# (<tf.Tensor 'SparseTensor_4/indices:0' shape=(2, 2) dtype=int64>,
+# <tf.Tensor 'SparseTensor_4/values:0' shape=(2,) dtype=int32>,
+# <tf.Tensor 'SparseTensor_4/dense_shape:0' shape=(2,) dtype=int64>)
  
  """
 represents the dense tensor
@@ -15,7 +15,7 @@ represents the dense tensor
 """
  
  tf.sparse_tensor_to_dense(st)
- <tf.Tensor 'SparseToDense:0' shape=(3, 4) dtype=int32>
+# <tf.Tensor 'SparseToDense:0' shape=(3, 4) dtype=int32>
 
 tf.sparse_to_dense(st.indices, st.dense_shape, st.values)
-<tf.Tensor 'SparseToDense_1:0' shape=(3, 4) dtype=int32>
+# <tf.Tensor 'SparseToDense_1:0' shape=(3, 4) dtype=int32>
